b72d1009df0ae3083f9dbf07c23a753884767c5d
imanolortizfernandez <imanolof29@gmail.com>
2024-12-06 12:42:19 +0100

    feat: fonts

--- app/src/main/java/com/imanolortiz/mastermeme/ui/theme/Type.kt
+++ app/src/main/java/com/imanolortiz/mastermeme/ui/theme/Type.kt
@@ -1,34 +1,16 @@
 package com.imanolortiz.mastermeme.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.imanolortiz.mastermeme.R
 
-// Set of Material typography styles to start with
+var ManropeFontFamily = FontFamily(
+    Font(R.font.manrope_medium)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge = Typography().displayLarge.copy(fontFamily = ManropeFontFamily),
+    displayMedium = Typography().displayMedium.copy(fontFamily = ManropeFontFamily),
+    displaySmall = Typography().displaySmall.copy(fontFamily = ManropeFontFamily),
 )
